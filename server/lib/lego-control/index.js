71630e210ee32e1c75d209d5dfe3b85ea6561d11
giaat <guillaume.peria@gmail.com>
2016-03-12 20:30:13 +0100

    added lego command functions

--- server/lib/lego-control/index.js
+++ server/lib/lego-control/index.js
@@ -5,50 +5,137 @@ var ev3dev = require('ev3dev-lang');
 var motorA = new ev3dev.Motor(ev3dev.OUTPUT_A);
 var motorB = new ev3dev.Motor(ev3dev.OUTPUT_B);
 
-if (!motorA.connected) {
-    console.error("No motor was found on port A. Please connect a tacho motor to port A and try again.");
-    process.exit(1);
-}
-
-if (!motorA.connected) {
-    console.error("No motor was found on port B. Please connect a tacho motor to port B and try again.");
-    process.exit(1);
-}
-
 // Prevent Node from exiting until motor is done
 var cancellationToken = setInterval(function() {
     if(motor.state.indexOf("running") == -1)
         clearInterval(cancellationToken);
 }, 10);
 
+function checkMotorConnected (letter) {
+    if (!"motor"+letter.connected) {
+        console.error("No motor was found on port " + letter + ". Please connect a tacho motor and try again.");
+        process.exit(1);
+    }
+}
+
 function up() {
+
+    // check if motors are connected
+    checkMotorConnected("A");
+    checkMotorConnected("B");
+
+    // enable speed regulation
     motorA.speedRegulationEnabled = 'on';
     motorB.speedRegulationEnabled = 'on';
 
+    // set motors speed
     motorA.speedSp = 500;
     motorB.speedSp = 500;
 
+    // set the amount of time the motor will run
+    motorA.timeSp = 400;
+    motorB.timeSp = 400;
+
+    // stop all previous command
     motorA.stopCommand = 'brake';
     motorB.stopCommand = 'brake';
 
-    motorA.positionSp = 4000;
-    motorB.positionSp = 4000;
+    // run the motor for the amount of time specified in `time_sp`
+    motorA.command = "run-timed";
+    motorB.command = "run-timed";
 
-    motorA.command = "run-to-rel-pos";
-    motorB.command = "run-to-rel-pos";
-
-    console.log("Running the motors A and B for 4000 tacho counts...");
+    console.log("Running forward the motors A and B for 4 sec ...");
 
 }
 
 function down() {
 
+    // check if motors are connected
+    checkMotorConnected("A");
+    checkMotorConnected("B");
+
+    // enable speed regulation and set motors speed
+    motorA.speedRegulationEnabled = 'on';
+    motorA.speedSp = 500;
+    motorB.speedRegulationEnabled = 'on';
+    motorB.speedSp = 500;
+
+    // set inverse polarity to move back
+    motorA.polarity = 'inversed';
+    motorB.polarity = 'inversed';
+
+    // set the amount of time the motor will run
+    motorA.timeSp = 400;
+    motorB.timeSp = 400;
+
+    // stop all previous command
+    motorA.stopCommand = 'brake';
+    motorB.stopCommand = 'brake';
+
+    // use var positionSp
+    motorA.command = "run-timed";
+    motorB.command = "run-timed";
+
+    console.log("Running back the motors A and B for 4 sec ...");
 }
 
 function left() {
 
+    // check if motors are connected
+    checkMotorConnected("A");
+    checkMotorConnected("B");
+
+    // enable speed regulation and set motors speed
+    motorA.speedRegulationEnabled = 'on';
+    motorA.speedSp = 500;
+    motorB.speedRegulationEnabled = 'on';
+    motorB.speedSp = 500;
+
+    // set inverse polarity to move back
+    motorA.polarity = 'inversed';
+
+    // set the amount of time the motor will run
+    motorA.timeSp = 400;
+    motorB.timeSp = 400;
+
+    // stop all previous command
+    motorA.stopCommand = 'brake';
+    motorB.stopCommand = 'brake';
+
+    // use var positionSp
+    motorA.command = "run-timed";
+    motorB.command = "run-timed";
+
+    console.log("Running back the motors A and B for 4 sec ...");
+
 }
 
 function right() {
+    // check if motors are connected
+    checkMotorConnected("A");
+    checkMotorConnected("B");
+
+    // enable speed regulation and set motors speed
+    motorA.speedRegulationEnabled = 'on';
+    motorA.speedSp = 500;
+    motorB.speedRegulationEnabled = 'on';
+    motorB.speedSp = 500;
+
+    // set inverse polarity to move back
+    motorB.polarity = 'inversed';
+
+    // set the amount of time the motor will run
+    motorA.timeSp = 400;
+    motorB.timeSp = 400;
+
+    // stop all previous command
+    motorA.stopCommand = 'brake';
+    motorB.stopCommand = 'brake';
+
+    // use var positionSp
+    motorA.command = "run-timed";
+    motorB.command = "run-timed";
+
+    console.log("Running back the motors A and B for 4 sec ...");
 
 }
