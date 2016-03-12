cfe8d57ffa10e6b15edeb95e09bbe2884796da1e
Timothe BORDIGA <tbordiga@dixeed.com>
2016-03-12 16:33:40 +0100

    Initial server commit

---
+++ server/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+
+module.exports = server;
+
+/////////////////////////////////////////////////////////////////////////
+
+server.connection({
+    host: 'localhost',
+    port: '8990'
+});
+
+////////////////////////////////////////////////////////////////////
+//                                                                //
+//                      Plugins registration                      //
+//                                                                //
+////////////////////////////////////////////////////////////////////
+server.register([
+])
+
+/////////////////////////////////////////////////////////////////////////
+//                                                                     //
+//                      Plugins configuration                          //
+//                                                                     //
+/////////////////////////////////////////////////////////////////////////
+.then(function () {
+    
+})
+.catch(function (err) {
+    console.error('Failed to load a plugin: ', err);
+    process.exit(2);
+})
+
+////////////////////////////////////////////////////////////////////
+//                                                                //
+//                      Start the server                          //
+//                                                                //
+////////////////////////////////////////////////////////////////////
+.then(function() {
+    return server.start();
+})
+.then(function() {
+    console.log('Server started on port : [' + config.get('/server/port') + ']');
+})
+.catch(function(err) {
+    console.error('Started server with errors: ' + err);
+    process.exit(3);
+});
